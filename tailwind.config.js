7fdbe98b179524ede95e3ddcf38cb74baa719257
infosecsujay <infosecsujay@gmail.com>
2025-12-29 02:00:26 +0530

    Fix Tailwind purge paths for static export

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
